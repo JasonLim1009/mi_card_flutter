81c4776bc3f8a0365679110432e88d9b6ebe79e7
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-09-10 20:03:51 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,72 +10,102 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/aida.jpg'),
-              ),
-                Text('Jason Lim',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+        backgroundColor: Colors.lightBlueAccent,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF701ebd),
+                Color(0xFF873bcc),
+                Color(0xFFfe4a97),
+                Color(0xFFe17763),
+                Color(0xFF68998c),
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/aida.jpg'),
                 ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.teal.shade100,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  width: 150.0,
-                  child: Divider(
-                    color: Colors.teal.shade100,
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(Icons.phone,
-                      color: Colors.teal,
+                  Text('Aida',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                    title: Text('+123 12 12 1234',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(Icons.person,
+                        color: Colors.grey,
+                      ),
+                      title: Text('Your Name',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text('xllao24@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(Icons.email,
+                        color: Colors.grey,
+                      ),
+                      title: Text('E-mail',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-            ],
-        )),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(Icons.drive_file_rename_outline,
+                        color: Colors.grey,
+                      ),
+                      title: Text('Your Message',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+          )),
+        ),
       ),
     );
   }
